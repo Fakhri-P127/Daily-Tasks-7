00e79af3c871d56493644036dd5697f036d0b6c6
Fakhri-P127 <fakhrita@code.edu.az>
2022-02-22 01:23:16 +0400

    added Access-Modifiers folder

---
+++ Access-Modifiers/Access-Modifiers/Group.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Access_Modifiers
+{
+    class Group
+    {
+        public string No;
+        public int Limit;
+        private Student[] _students;       
+        public Group(string no, int limit)
+        {
+            _students = new Student[0];
+            No = no;
+            Limit = limit;                        
+        }
+        public void AddStudent(Student student)
+        {
+            if(_students.Length < Limit) 
+            {
+                Array.Resize(ref _students, _students.Length + 1);
+                _students[_students.Length-1] = student;          
+            }
+            else
+            {
+                Console.WriteLine("Group has reached it's limit");                
+            }           
+        }
+        public Student[] GetStudens()
+        {
+            return _students;
+        }
+    }
+}
---
+++ Access-Modifiers/Access-Modifiers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Access_Modifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Student student1 = new Student("Fakhri","Afandiyev");
+            Student student2 = new Student("Kamran", "Abilov");
+            Student student3 = new Student("Gunel", "Soyadi");
+            Student student4 = new Student("Cahangir", "Soyadi");
+            Student student5 = new Student("Leon", "Kennedy");
+            Group group = new Group("P127", 4);      
+            group.AddStudent(student1);
+            group.AddStudent(student2);
+            group.AddStudent(student3);
+            group.AddStudent(student4);
+            group.AddStudent(student5);
+            Student[] students = group.GetStudens();        
+            
+            foreach (Student student in students)
+            {                              
+                Console.WriteLine(student.GetFullname());
+            }            
+        }
+    }
+}
